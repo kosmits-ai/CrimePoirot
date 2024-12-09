09cf53470ec24441d4b0415b2fb053986fff5011
kosmits-ai <kmitsionis@gmail.com>
2024-12-09 14:28:51 +0200

    prevent printing duplicated documents from db

--- CrimePoirot/view_results.py
+++ CrimePoirot/view_results.py
@@ -49,6 +49,7 @@ def check_findings(collection, repo_name, zero_field, description_fields):
             print()
         print()
 
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: python script.py <repo_url>")
@@ -68,22 +69,44 @@ if __name__ == "__main__":
 
     # Check Guarddog findings
     guarddog_collection = connect_to_mongo("guarddog_reports")
-    check_findings(
-        guarddog_collection,
-        repo_name,
-        "output_text",
-        ["output_text", "rule_id"]
-    )
+   
+    guarddog_collection = connect_to_mongo("guarddog_reports")
+    matching_document = guarddog_collection.find_one({"repo_name":repo_name , "output_text":0})
+    if matching_document:
+        print(f"No findings found in guarddog.reports for repository: {repo_name}")
+    else:
+        seen = set()  # Set to track printed documents
+        findings = guarddog_collection.find({"repo_name": repo_name})
+        print("Findings in safety.reports :")
+        for doc in findings:
+                # Create a unique identifier for the finding
+            unique_key = (repo_name, doc.get("output_text", "N/A"))
+            if unique_key not in seen:
+                seen.add(unique_key)
+                print(f"Package name: {doc.get('output_text', 'N/A')}")
+                print(f"Package name: {doc.get('rule_id', 'N/A')}")
+                print()
+        print()
+
+
 
     # Check Safety findings
     safety_collection = connect_to_mongo("safety_reports")
-    check_findings(
-        safety_collection,
-        repo_name,
-        "output",
-        ["package_name"]
-    )
-
+    matching_document = safety_collection.find_one({"repo_name":repo_name , "output":0})
+    if matching_document:
+        print(f"No findings found in safety.reports for repository: {repo_name}")
+    else:
+        seen = set()  # Set to track printed documents
+        findings = safety_collection.find({"repo_name": repo_name})
+        print("Findings in safety.reports :")
+        for doc in findings:
+                # Create a unique identifier for the finding
+            unique_key = (repo_name, doc.get("package_name", "N/A"))
+            if unique_key not in seen:
+                seen.add(unique_key)
+                print(f"Package name: {doc.get('package_name', 'N/A')}")
+                print()
+        print()
     # Check Bearer findings
     bearer_collection = connect_to_mongo("bearer_reports")
     bearer_document = bearer_collection.find_one({"repository": repo_name})
